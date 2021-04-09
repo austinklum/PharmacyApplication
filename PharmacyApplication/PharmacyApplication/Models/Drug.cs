07ace9b2aa180c1433b6024ab2c0a41039cbcf22
DanielPierce <danny@pierceplace.net>
2021-04-09 16:46:29 -0500

    Model

---
+++ PharmacyApplication/PharmacyApplication/Models/Drug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Models
+{
+    public class Drug
+    {
+        public int Id { get; set; }
+        public string MedicalName { get; set; }
+        public string Type { get; set; }
+        public int Stock { get; set; }
+        public double CostPer { get; set; }
+        public string RecommendedDose { get; set; }
+    }
+}
