2257244c03d93e40b1866995fb8490ba43585df1
DanielPierce <danny@pierceplace.net>
2021-04-09 17:15:52 -0500

    Context

---
+++ PharmacyApplication/PharmacyApplication/Data/VerificationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Data
+{
+    public class VerificationContext : DbContext
+    {
+        public VerificationContext(DbContextOptions<VerificationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<VerifiedPatient> Patients { get; set; }
+        public DbSet<VerifiedPhysician> Physicians { get; set; }
+    }
+}
