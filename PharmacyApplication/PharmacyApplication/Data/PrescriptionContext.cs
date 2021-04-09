2b401088fc1caba2ff64ac13f18a7b8219ebc321
DanielPierce <danny@pierceplace.net>
2021-04-09 17:34:29 -0500

    Context

---
+++ PharmacyApplication/PharmacyApplication/Data/PrescriptionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Data
+{
+    public class PrescriptionContext : DbContext
+    {
+        public PrescriptionContext(DbContextOptions<PrescriptionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<PrescribedDrug> PrescribedDrugs { get; set; }
+    }
+}
