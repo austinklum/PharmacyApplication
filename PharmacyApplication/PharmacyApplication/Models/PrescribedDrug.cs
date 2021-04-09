43b1be2d08b4f6c9373c3dbbcad57064f33363da
DanielPierce <danny@pierceplace.net>
2021-04-09 17:31:22 -0500

    Models

---
+++ PharmacyApplication/PharmacyApplication/Models/PrescribedDrug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Models
+{
+    public class PrescribedDrug
+    {
+        public int Id { get; set; }
+        public int DrugId { get; set; }
+        public int PrescriptionId { get; set; }
+        public int Count { get; set; }
+        public string Dosage { get; set; }
+        [DisplayName("Refill Count")]
+        public int RefillCount { get; set; }
+    }
+}
---
+++ PharmacyApplication/PharmacyApplication/Models/Prescription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Models
+{
+    public class Prescription
+    {
+        public int Id { get; set; }
+        [DisplayName("Physician Name")]
+        public string PhysicianName { get; set; }
+        [DisplayName("Physician License Number")]
+        public string PhysicianLicenseNumber { get; set; }
+        [DisplayName("Patient Name")]
+        public string PatientName { get; set; }
+        [DisplayName("Patient DOB")]
+        public DateTime PatientDOB { get; set; }
+        [DisplayName("Patient Address")]
+        public string PatientAddress { get; set; }
+        [DisplayName("Issued on")]
+        public DateTime IssuedDate { get; set; }
+    }
+}
