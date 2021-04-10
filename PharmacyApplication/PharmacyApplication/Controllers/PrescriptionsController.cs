b6d7d3ddc3fc9fb3102447daefef1b2348d1dba1
DanielPierce <danny@pierceplace.net>
2021-04-10 15:13:23 -0500

    Basic views

---
+++ PharmacyApplication/PharmacyApplication/Controllers/PrescriptionsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PharmacyApplication.Data;
+using PharmacyApplication.Models;
+
+namespace PharmacyApplication.Controllers
+{
+    public class PrescriptionsController : Controller
+    {
+        private readonly PrescriptionContext _context;
+
+        public PrescriptionsController(PrescriptionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Prescriptions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Prescriptions.ToListAsync());
+        }
+
+        // GET: Prescriptions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prescription = await _context.Prescriptions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+
+            return View(prescription);
+        }
+
+        private bool PrescriptionExists(int id)
+        {
+            return _context.Prescriptions.Any(e => e.Id == id);
+        }
+    }
+}
