a6dd1daa3b3ef92f00f88ac2e1bcdd73815ea787
DanielPierce <danny@pierceplace.net>
2021-04-18 13:27:57 -0500

    Component for prescriptions on dashboard

---
+++ PharmacyApplication/PharmacyApplication/Components/PrescriptionListViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PharmacyApplication.Data;
+using PharmacyApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Components
+{
+    public class PrescriptionListViewComponent : ViewComponent
+    {
+        private readonly PrescriptionContext _prescriptionContext;
+        private readonly VerificationContext _verificationContext;
+        private readonly DrugContext _drugContext;
+
+        public PrescriptionListViewComponent(PrescriptionContext prescriptionContext, DrugContext drugContext, VerificationContext verificationContext)
+        {
+            _prescriptionContext = prescriptionContext;
+            _drugContext = drugContext;
+            _verificationContext = verificationContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Prescription> prescriptions = (from p in _prescriptionContext.Prescriptions select p).ToList();
+
+            prescriptions = prescriptions.Where(p => p.BillCreated == false).ToList();
+
+            PrescriptionsViewModel vm = new PrescriptionsViewModel
+            {
+                Prescriptions = prescriptions,
+                IncludeProcessed = false,
+            };
+            HttpContext.Session.SetString("PrescriptionFillValidation", "");
+            return View(vm);
+        }
+    }
+}
