2f2df6d8d23beb559a7e608cf394f40b91a9342d
DanielPierce <danny@pierceplace.net>
2021-04-24 01:09:45 -0500

    Better formatting, pass id

--- PharmacyApplication/PharmacyApplication/Models/User.cs
+++ PharmacyApplication/PharmacyApplication/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,13 @@ namespace PharmacyApplication.Models
         public int Id { get; set; }
         public String Username { get; set; }
         public String Password { get; set; }
+        [DisplayName("Security Question 1")]
         public int SecQ1Index { get; set; }
         public String SecQ1Response { get; set; }
+        [DisplayName("Security Question 2")]
         public int SecQ2Index { get; set; }
         public String SecQ2Response { get; set; }
+        [DisplayName("Security Question 3")]
         public int SecQ3Index { get; set; }
         public String SecQ3Response { get; set; }
         public int AccountStatus { get; set; }
