12dd06c44ca01dc45f126a55ba660c05c4c5e297
DanielPierce <danny@pierceplace.net>
2021-05-03 05:26:25 -0500

    physicians

--- PharmacyApplication/PharmacyApplication/Controllers/VerifiedPhysiciansController.cs
+++ PharmacyApplication/PharmacyApplication/Controllers/VerifiedPhysiciansController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -31,6 +32,10 @@ namespace PharmacyApplication.Controllers
             {
                 physicians = physicians.Where(p => p.Name.Contains(searchString));
             }
+            HttpContext.Session.SetString(HomeController.CreatePatientNameValidation, "");
+            HttpContext.Session.SetString(HomeController.CreatePatientAddressValidation, "");
+            HttpContext.Session.SetString(HomeController.CreatePatientPronounsValidation, "");
+            HttpContext.Session.SetString(HomeController.CreatePatientDOBValidation, "");
             return View(await physicians.ToListAsync());
         }
 
@@ -68,6 +73,51 @@ namespace PharmacyApplication.Controllers
         private bool VerifiedPhysicianExists(int id)
         {
             return _verificationContext.VerifiedPhysicians.Any(e => e.Id == id);
+        }
+
+        public IActionResult Create()
+        {
+            VerifiedPhysician vp = new VerifiedPhysician();
+            return View(vp);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(VerifiedPhysician vp)
+        {
+            HttpContext.Session.SetString(HomeController.CreatePatientNameValidation, "");
+            HttpContext.Session.SetString(HomeController.CreatePatientAddressValidation, "");
+            HttpContext.Session.SetString(HomeController.CreatePatientPronounsValidation, "");
+            HttpContext.Session.SetString(HomeController.CreatePatientDOBValidation, "");
+
+            if (string.IsNullOrEmpty(vp.Name))
+            {
+                HttpContext.Session.SetString(HomeController.CreatePatientNameValidation, "Required Field");
+                return View(vp);
+            }
+            if (string.IsNullOrEmpty(vp.LicenseNumber))
+            {
+                HttpContext.Session.SetString(HomeController.CreatePatientDOBValidation, "Required Field");
+                return View(vp);
+            }
+            if (string.IsNullOrEmpty(vp.Pronouns))
+            {
+                HttpContext.Session.SetString(HomeController.CreatePatientPronounsValidation, "Required Field");
+                return View(vp);
+            }
+
+
+            _verificationContext.Add(vp);
+            await _verificationContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            VerifiedPhysician vp = _verificationContext.VerifiedPhysicians.FirstOrDefault(p => p.Id == id);
+            _verificationContext.Remove(vp);
+            _verificationContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
